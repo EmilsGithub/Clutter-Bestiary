29ad80d82b9e200733e1042448d901d56f16f1a2
EmilSG <emilsvardgronvikacc@gmail.com>
2026-01-07 21:51:33 +0100

    # Clutter: Bestiary 1.1.1 :Fabric: :NeoForge: With just a bugfix, here is the release for Clutter: Bestiary 1.1.1, for 1.21.1, Neoforge and Fabric. ### Added: - N/A ### Changed / TLC: - N/A
    ### Fixed:
    - "Seahorse in a Bucket" item not actually spawning a Seahorse

--- common/src/main/java/net/emilsg/clutterbestiary/item/custom/SeahorseBucketItem.java
+++ common/src/main/java/net/emilsg/clutterbestiary/item/custom/SeahorseBucketItem.java
@@ -2,10 +2,22 @@ package net.emilsg.clutterbestiary.item.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.emilsg.clutterbestiary.entity.variants.SeahorseVariant;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.entity.Bucketable;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.EntityBucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
@@ -18,4 +30,20 @@ public class SeahorseBucketItem extends EntityBucketItem {
         this.type = type;
     }
 
+    public void onEmptied(@Nullable PlayerEntity player, World world, ItemStack stack, BlockPos pos) {
+        if (world instanceof ServerWorld) {
+            this.spawnEntity((ServerWorld) world, stack, pos);
+            world.emitGameEvent(player, GameEvent.ENTITY_PLACE, pos);
+        }
+    }
+
+    private void spawnEntity(ServerWorld world, ItemStack stack, BlockPos pos) {
+        Entity entity = this.type.get().spawnFromItemStack(world, stack, null, pos, SpawnReason.BUCKET, true, false);
+        if (entity instanceof Bucketable bucketable) {
+            NbtComponent nbtComponent = stack.getOrDefault(DataComponentTypes.BUCKET_ENTITY_DATA, NbtComponent.DEFAULT);
+            bucketable.copyDataFromNbt(nbtComponent.copyNbt());
+            bucketable.setFromBucket(true);
+        }
+
+    }
 }
